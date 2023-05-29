82985880ef246c9399dbf0c9947cd5cab3857319
kawakionrails <erick_nara@icloud.com>
2023-05-29 09:46:32 -0300

    feat: navigation component and home screen

---
+++ app/src/main/java/br/com/datehunter/presenter/fragments/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package br.com.datehunter.presenter.fragments.home
+
+import br.com.datehunter.presenter.base.BaseViewModel
+
+class HomeViewModel : BaseViewModel() {
+
+    // TODO: Not yet implemented
+
+}
--- app/src/main/java/br/com/datehunter/presenter/fragments/splash/SplashViewModel.kt
+++ app/src/main/java/br/com/datehunter/presenter/fragments/splash/SplashViewModel.kt
@@ -1,4 +1,9 @@
 package br.com.datehunter.presenter.fragments.splash
 
-class SplashViewModel {
+import br.com.datehunter.presenter.base.BaseViewModel
+
+class SplashViewModel : BaseViewModel() {
+
+    // TODO: Not yet implemented
+
 }
